4dce28e40e137639747dac04444fdeb8bb406b03
JungHyunKwon <kjh_3859@naver.com>
2019-02-25 10:34:49 +0900

    published:

--- js/getCSS.js
+++ js/getCSS.js
@@ -3,26 +3,8 @@
  * @version 1.0.0
  */
 try {
-	(function(html) {
+	(function() {
 		'use strict';
-		
-		/**
-		 * @name 요소 확인
-		 * @since 2019-01-18
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		function _isElement(value) {
-			var result = false;
-			
-			try {
-				result = html.contains(value);
-			}catch(e) {
-				//throw e;
-			}
-
-			return result;
-		}
 		
 		/**
 		 * @name getCSS
@@ -33,14 +15,15 @@ try {
 		window.getCSS = function(element) {
 			var result = {};
 			
-			//요소일 때
-			if(_isElement(element)) {
+			try {
 				result = element.currentStyle || getComputedStyle(element);
+			}catch(e) {
+				//throw e;
 			}
 
 			return result;
 		};
-	})(document.documentElement);
+	})();
 }catch(e) {
 	console.error(e);
 }
